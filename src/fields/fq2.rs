77f8a3f5d8576137a8c316d5781cdb724441b8fa
Tim Zerrell <tim.zerrell@gmail.com>
2025-02-13 16:15:54 -0800

    Fix alignment issue (#3)

--- src/fields/fq2.rs
+++ src/fields/fq2.rs
@@ -162,10 +162,14 @@ impl Mul for Fq2 {
         let prime: [u32; 8] = bytemuck::cast(Fq::modulus().0);
         let prime_sqr: [u32; 16] = bytemuck::cast(Fq::modulus_squared().0);
 
-        let mut result_mut = [[0u32; 8]; 2];
+        // We construct our result using u32s but will eventually need to interpret it as u128s, so
+        // we align to the u128 alignment (i.e. of 16 bytes)
+        #[repr(align(16))]
+        struct AlignedU32s([[u32; 8]; 2]);
+        let mut result_mut = AlignedU32s([[0u32; 8]; 2]);
 
-        field::extfield_xxone_mul_256(&lhs, &rhs, &prime, &prime_sqr, &mut result_mut);
-        let result: &[[u128; 2]; 2] = bytemuck::cast_ref(&result_mut);
+        field::extfield_xxone_mul_256(&lhs, &rhs, &prime, &prime_sqr, &mut result_mut.0);
+        let result: &[[u128; 2]; 2] = bytemuck::cast_ref(&result_mut.0);
         Fq2 {
             c0: Fq::new(U256(result[0])).unwrap(),
             c1: Fq::new(U256(result[1])).unwrap(),
